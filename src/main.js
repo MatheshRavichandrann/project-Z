ba8e53b9d070f98688544b5a60586bc9fc1e8046
Mathesh <mathesh1907@gmail.com>
2025-03-10 00:22:27 +0530

    add style

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import '../styles/morden-normaize.css'
 import '../styles/style.css'
 import '../styles/components/header.css'
+import '../styles/components/hero.css'
+import '../styles/components/about.css'
 import '../styles/utils.css'
